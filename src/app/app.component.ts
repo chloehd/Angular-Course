0d8188b0ea34a77106f6d835676e6b902d68a5f6
jo-chloe <c.hipeau@jo-media.net>
2020-02-27 11:57:16 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'CourseOC-Angular';
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
+
+  appareils = [
+    {
+      name: "Machine à laver",
+      status: "éteint"
+    },
+    {
+      name: "Réfrigérateur",
+      status: "allumé"
+    },
+    {
+      name: "Ordinateur",
+      status: "éteint"
+    }
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+
+  onAllumer() {
+    console.log("On allume tout!");
+  }
 }
